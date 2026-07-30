67f5a40408f3c688ea18b94dddb16aad863fcb58
RBhope <rbhope@unomaha.edu>
2026-07-29 20:05:48 -0500

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prod=[1]*len(nums)
+        ## prod = [1,1,1,1,1]
+        for i in range (len(nums)):
+            for j in range (len(nums)):
+                if i!=j:
+                    prod[i] = prod[i]*nums[j]
+        return prod
+
+        
