6e331b115841ebcdb60e062446ece9ad0911fec5
RBhope <rbhope@unomaha.edu>
2026-09-04 20:43:24 -0500

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        memo = {}
+        
+        def can_reach_from(position: int) -> bool:
+            if position >= len(nums) - 1:
+                return True
+                
+            if position in memo:
+                return memo[position]
+                
+            max_jump = nums[position]
+            
+            for jump in range(1, max_jump + 1):
+                next_position = position + jump
+                if can_reach_from(next_position):
+                    memo[position] = True
+                    return True
+                    
+            memo[position] = False
+            return False
+            
+        return can_reach_from(0)
