c229314bb5ed74e505bb9268000f170a038244d5
RBhope <rbhope@unomaha.edu>
2026-09-21 19:48:28 -0700

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        count=[0]*(n+1)
+        for i in range(n+1):
+            bin_n = str(bin(i))
+            count[i] = 0
+            for char in bin_n:
+                if char == "1":
+                    count[i]+=1
+        
+        return count
